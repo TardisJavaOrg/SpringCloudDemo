1311aae399a0fdc46e1a447d129ac34a9f6cae6e
Tardis <yanggz@rynnova.com>
2021-03-20 19:51:01 +0800

    add eureka client

--- eureka-server/src/test/java/cn/tardis/cloud/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/cn/tardis/cloud/EurekaServerApplicationTests.java
@@ -3,6 +3,9 @@ package cn.tardis.cloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootTest
 class EurekaServerApplicationTests {
 
